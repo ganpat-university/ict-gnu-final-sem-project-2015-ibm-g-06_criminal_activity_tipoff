8d04a351120ba5edeb34f10cb3d39b69c0b9cd6f
jagirshastri <56964345+jagirshastri@users.noreply.github.com>
2023-04-28 00:31:37 +0530

    aeleted wrong files

--- tipoff/login_V1/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path,re_path,include
-from .views import *
-urlpatterns = [
-    path('login/',login_page,name="login"),
-    # path('register/',register,name="register"),
-    path('logout/',logout_user,name="logout"),
-]
